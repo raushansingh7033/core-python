bc00bbadde83200a2af02151aeb844a0e881feb4
raushansingh7033 <raushan786267@gmail.com>
2026-04-03 23:24:58 +0530

    practice

--- practice/_01_Basics.py
+++ practice/_01_Basics.py
@@ -1,1 +1,16 @@
 print("raushan")
+a=10
+b=20
+print(a)
+print(b)
+
+print((10,20,30))
+print([20,30,])
+print({10,20})
+print({'name':"raushan",10:10})
+
+first=int(input("Enter first number"))
+second=int(input("Enter second number"))
+
+sum=first+second
+print(sum)
